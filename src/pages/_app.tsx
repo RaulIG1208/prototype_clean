aad1a90522ebeee0bfd3281929d18562c7d5cd86
Raul Gonzalez <rvega@tesacom.net>
2021-11-18 17:15:56 -0300

    Add next-intl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../Mui/theme";
+import createEmotionCache from "../Mui/crateEmotionCache";
+import { NextIntlProvider } from "next-intl";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <NextIntlProvider
+        formats={{
+          dateTime: {
+            short: {
+              day: "numeric",
+              month: "short",
+              year: "numeric",
+            },
+          },
+        }}
+        messages={pageProps.messages}
+        now={new Date(pageProps.now)}
+      >
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </NextIntlProvider>
+    </CacheProvider>
+  );
+}
